04396b53c8cbbed73cd38ef4ed74081d70fe645a
Adelina4343 <pavaladelina7@gmail.com>
2025-02-01 21:46:05 +0200

    FinalCommit

--- results-summary-component-main/script.js
+++ results-summary-component-main/script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const summaryItemsContainer = document.getElementById('summaryItems');
+  const summaryItemsContainer = document.getElementById('summaryItemsContainer');
+  const avgElement = document.querySelector('.score');
+  const assessmentText = document.querySelector('.assessment .assessment-text');
 
-  // 1. Debugging: Log the current directory to check the relative path
-  console.log("Current directory:", window.location.href); // Or document.URL
-
-  // 2. Fetch the JSON (Corrected Path - SEE BELOW!)
-  fetch('data.json') // Or fetch('data.json') if in the same folder
+  if (!summaryItemsContainer || !avgElement){
+    console.error("Elementul cu ID_ul 'summaryItemsContainer' nu a fost gasit");
+      return;
+  }
+  fetch('data.json')
     .then(response => {
       if (!response.ok) { // Check for HTTP errors (404, 500, etc.)
         throw new Error(`HTTP error! status: ${response.status}`); // Throw error for .catch
@@ -13,7 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
       return response.json();
     })
     .then(jsonData => {
+      let totalValue = 0;
+
       jsonData.forEach(item => {
+        totalValue += item.score;
+
         const summaryItem = document.createElement('div');
         summaryItem.classList.add('summary-item', item.category.toLowerCase());
 
@@ -29,6 +35,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
         summaryItemsContainer.appendChild(summaryItem);
       });
+      const avregeVal = Math.round(totalValue / 4);   // Manage average of the total score
+      avgElement.textContent = avregeVal;
+      if (avregeVal <= 65){
+        assessmentText.classList.add("hidden");     // Hide message when avg of score is less that 65
+      }
     })
     .catch(error => {
       console.error('Error fetching JSON:', error);
